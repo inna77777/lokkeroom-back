bca2d8ee8d4468018938b197b3a5cc23fb4916bc
Inna Kobets <inna42320@gmail.com>
2024-04-24 18:32:23 +0200

    added remote db

--- db.js
+++ db.js
@@ -1,10 +1,10 @@
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  user: "postgres",
-  password: "user",
-  host: "localhost",
-  database: "postgres",
+  user: "ue73hnca7qflqs",
+  password: "pefb246349c5f81b5d05d52b9d4e93e5b232229b331a909377c921ce574fe2981",
+  host: "cdgn4ufq38ipd0.cluster-czz5s0kz4scl.eu-west-1.rds.amazonaws.com",
+  database: "dbs5m0dggne3qf",
   port: 5432,
 });
 
