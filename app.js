69babefbdb714e4faa275f3404935d804098b437
Inna Kobets <inna42320@gmail.com>
2024-04-27 00:16:52 +0200

    hdbhb

--- app.js
+++ app.js
@@ -13,6 +13,13 @@ app.use(cors());
 
 app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
+
 app.use(authRoutes);
 app.use(messageRoutes);
 app.use(lobbyRoutes);
